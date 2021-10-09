5ce7c47044da91dd95096e8d49b944730e0c54bd
Raghav Arora <65889311+Rghv100@users.noreply.github.com>
2021-10-09 23:50:14 +0530

    second commit
    Added user and post end points, connected mongo db

--- main.go
+++ main.go
@@ -1,89 +1,124 @@
-
-// main.go
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "io/ioutil"
-    "net/http"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+    "golang.org/x/crypto/bcrypt"
 )
 
-// Article - Our struct for all articles
-type Article struct {
-    Id      string    `json:"Id"`
-    Title   string `json:"Title"`
-    Desc    string `json:"desc"`
-    Content string `json:"content"`
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"Name,omitempty" bson:"Name,omitempty"`
+	Email    string             `json:"Email,omitempty" bson:"Email,omitempty"`
+	Password string             `json:"Password,omitempty" bson:"Password,omitempty"`
 }
 
-var Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+type Posts struct {
+	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Caption         string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	ImgURL        string             `json:"ImgURL,omitempty" bson:"ImgURL,omitempty"`
+	PostedTimestamp string             `json:"PostedTimeStamp,omitempty" bson:"PostedTimestamp,omitempty"`
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(Articles)
-}
+var client *mongo.Client
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
-
-    for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
+//function for connecting to mongoDb
+func ConnectMongo() {
+	var mongoURL = "mongodb+srv://Raghav:<Raghav1024>@appointydb.pxglp.mongodb.net/AppointyDB?retryWrites=true&w=majority"
+    client, err := mongo.NewClient(options.Client().ApplyURI(mongoURL))
+    ctx,_:= context.WithTimeout(context.Background(), 10*time.Second)
+    err = client.Connect(ctx)
+    ctx,_= context.WithTimeout(context.Background(), 10*time.Second)
+    if err = client.Ping(ctx, readpref.Primary()); err != nil {
+        fmt.Println("FAILED TO CONNECT TO MONGODB : %v\n", err)
+        return
     }
-}
+    fmt.Println("SUCCESSFULLY CONNECTED TO MONGODB DATABASE :", mongoURL)
 
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-    // get the body of our POST request
-    // unmarshal this into a new Article struct
-    // append this to our Articles array.    
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var article Article 
-    json.Unmarshal(reqBody, &article)
-    // update our global Articles array to include
-    // our new Article
-    Articles = append(Articles, article)
-
-    json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
-
-    for index, article := range Articles {
-        if article.Id == id {
-            Articles = append(Articles[:index], Articles[index+1:]...)
-        }
-    }
 
 }
 
-func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/articles", returnAllArticles)
-    myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-    myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-    myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-    log.Fatal(http.ListenAndServe(":10000", myRouter))
+//function to Create an User
+func CreateUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	if err != nil || hashedPassword != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	collection := client.Database("AppointyDB").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+// Function to Get a user using id
+func GetUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("AppointyDB").Collection("user")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
+}
+
+//Function to Create a Post
+func CreatePost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var post Posts
+	_ = json.NewDecoder(request.Body).Decode(&post)
+	collection := client.Database("AppointyDB").Collection("post")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, post)
+	json.NewEncoder(response).Encode(result)
+}
+
+//Function to Get a post using id
+func GetPost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Posts
+	collection := client.Database("AppointyDB").Collection("post")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, Posts{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
 }
 
 func main() {
-    Articles = []Article{
-        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
-    handleRequests()
+	fmt.Println("Hello Appointy")
+	ConnectMongo()
+	router := mux.NewRouter()
+	router.HandleFunc("/user", CreateUser).Methods("POST")
+	router.HandleFunc("/user/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/post", CreatePost).Methods("POST")
+	router.HandleFunc("/post/{id}", GetPost).Methods("GET")
+	http.ListenAndServe(":10000", router)
 }
+
+
+
